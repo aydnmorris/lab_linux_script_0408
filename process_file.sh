a17ae95803d7e9e79fe4385c2be519fd687562a0
unknown <morrisaydn8@gmail.com>
2026-04-12 21:25:45 -0700

    linux status change

--- process_file.sh
+++ process_file.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+## What is the technical term for the code on Line 1
+## What does it accomplish
+
+#The technical term is shebang 
+#what shebang does is indicates the operating system what should be used to execute the file
+
+# ----------------------------
+# Simple File Processing Script
+# ----------------------------
+
+##Explain lines 11 - 13. What are they and where are they used later in the script?
+#Line 11 is to check whetrher the input file exists or not.
+#Line 13 will terminate the script and indicate an error message.
+#They are used in lines 11-14 to act as a safety check before processing.
 INPUT_FILE="input/test.txt"
+INPUT_FILE_1="input/aydn_test.txt"
 OUTPUT_DIR="output"
 OUTPUT_FILE="output/processed.txt"
 
